7c72c072079eb87ade5be4fda0d7bd7ed0a04a0c
Jon Ursenbach <jon@ursenba.ch>
2024-05-10 23:55:36 -0700

    feat: forking the upstream package and getting it up to date

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,11 @@
-/* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports */
-import { JsonSchemaDraft4, JsonSchemaDraft202012 } from "./json-schema";
-export { JsonSchemaDraft4, JsonSchemaDraft202012 };
+import openapiV1 from './schemas/v1.2/apiDeclaration.json';
+import openapiV2 from './schemas/v2.0/schema.json';
+import openapiV3 from './schemas/v3.0/schema.json';
+import openapiV31 from './schemas/v3.1/schema.json';
 
-/**
- * JSON Schema for OpenAPI Specification v1.2
- */
-export const openapiV1 = require("../schemas/v1.2/apiDeclaration.json") as JsonSchemaDraft4;
-
-/**
- * JSON Schema for OpenAPI Specification v2.0
- */
-export const openapiV2 = require("../schemas/v2.0/schema.json") as JsonSchemaDraft4;
-
-/**
- * JSON Schema for OpenAPI Specification v3.0
- */
-export const openapiV3 = require("../schemas/v3.0/schema.json") as JsonSchemaDraft4;
-
-/**
- * JSON Schema for OpenAPI Specification v3.1
- */
-export const openapiV31 = require("../schemas/v3.1/schema.json") as JsonSchemaDraft202012;
-
-/**
- * JSON Schemas for every version of the OpenAPI Specification
- */
 export const openapi = {
   v1: openapiV1,
   v2: openapiV2,
   v3: openapiV3,
   v31: openapiV31,
-};
-
-// Export `openapi` as the default export
-export default openapi;
-
-// CommonJS default export hack
-/* eslint-env commonjs */
-if (typeof module === "object" && typeof module.exports === "object") {
-  module.exports = Object.assign(module.exports.default, module.exports);
-}
+} as const;
---
+++ test/index.test.ts
@@ -0,0 +1,41 @@
+import { test, expect } from 'vitest';
+
+import { openapi } from '../src';
+
+function isJsonSchemaDraft4(obj: unknown) {
+  expect(obj).toBeTypeOf('object');
+  expect(obj).toHaveProperty('id');
+  expect(obj).toHaveProperty('$schema');
+  expect(obj).toHaveProperty('properties');
+  expect(obj).toHaveProperty('definitions');
+  return true;
+}
+
+function isJsonSchemaDraft202012(obj: unknown) {
+  expect(obj).toBeTypeOf('object');
+  expect(obj).toHaveProperty('$id');
+  expect(obj).toHaveProperty('$schema');
+  expect(obj).toHaveProperty('properties');
+  expect(obj).toHaveProperty('$defs');
+  return true;
+}
+
+test('should export the Swagger v1.2 schema', () => {
+  expect(openapi.v1).toSatisfy(isJsonSchemaDraft4);
+  expect(openapi.v1.properties.swaggerVersion.enum).toStrictEqual(['1.2']);
+});
+
+test('should export the Swagger v2 schema', () => {
+  expect(openapi.v2).toSatisfy(isJsonSchemaDraft4);
+  expect(openapi.v2.properties.swagger.enum).toStrictEqual(['2.0']);
+});
+
+test('should export the OpenAPI v3 schema', () => {
+  expect(openapi.v3).toSatisfy(isJsonSchemaDraft4);
+  expect(openapi.v3.properties.openapi.pattern).toBe('^3\\.0\\.\\d(-.+)?$');
+});
+
+test('should export the OpenAPI v3.1 schema', () => {
+  expect(openapi.v31).toSatisfy(isJsonSchemaDraft202012);
+  expect(openapi.v31.properties.openapi.pattern).toBe('^3\\.1\\.\\d+(-.+)?$');
+});
