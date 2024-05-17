09dc3d86960ee5092afd3c6fe86a66ba36df04c6
Kanad Gupta <git@kanad.dev>
2024-05-17 13:47:22 -0500

    feat: add processing script for legacy schema

---
+++ src/create-legacy-schema.ts
@@ -0,0 +1,13 @@
+import fs from 'node:fs';
+
+import asdf from './schemas/v3.1/schema.json';
+
+// we gotta fix that type woof
+const clone = structuredClone(asdf) as any;
+
+// we gotta make sure that this is the only actual change and that this change is actually correct
+clone.$defs.header.properties.schema = { type: ['object', 'boolean'] };
+
+fs.writeFileSync('src/schemas/v3.1/legacy-schema.json', JSON.stringify(clone, null, 2));
+
+console.log('legacy schema created!');
