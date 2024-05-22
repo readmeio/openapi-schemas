9e09dc60eb71be5d4d66c19f0737cec727fd5e12
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2024-05-22 15:46:36 -0500

    feat: post-processing script for v3.1 schema (#2)
    * chore(deps): install tsx
    
    * fix(build script): also clear out src/schemas
    
    * feat: add processing script for legacy schema
    
    * lint
    
    * refactor: update typings
    
    * refactor: update name
    
    * Update src/create-legacy-schema.ts
    
    ---------
    
    Co-authored-by: Darren Yong <contact.darrenyong@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import openapiV1 from './schemas/v1.2/apiDeclaration.json';
 import openapiV2 from './schemas/v2.0/schema.json';
 import openapiV3 from './schemas/v3.0/schema.json';
+import openapiV31Legacy from './schemas/v3.1/legacy-schema.json';
 import openapiV31 from './schemas/v3.1/schema.json';
 
 export const openapi = {
@@ -8,4 +9,5 @@ export const openapi = {
   v2: openapiV2,
   v3: openapiV3,
   v31: openapiV31,
+  v31legacy: openapiV31Legacy,
 } as const;
